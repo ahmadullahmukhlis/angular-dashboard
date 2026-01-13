703178ab26dbe60eb551e584b4407b07cb4f59ef
Ahmadullah Mukhlis <yourgmail@gmail.com>
2026-01-13 14:24:56 +0430

    the problem of the is fxed for the systatic car of the main

--- src/app/components/layout/main-content/main-content.ts
+++ src/app/components/layout/main-content/main-content.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Sidebar } from '../sidebar/sidebar';
 import { Header } from '../header/header';
+import { SidebarService } from '../../../services/sidebar.service';
 
 @Component({
   selector: 'app-main-content',
@@ -11,4 +12,21 @@ import { Header } from '../header/header';
   templateUrl: './main-content.html',
   styleUrls: ['./main-content.css'], // <-- fix here
 })
-export class MainContent {}
+export class MainContent {
+  constructor(private sidebarService: SidebarService) {}
+
+  isMobile = false;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.checkMobile();
+  }
+
+  private checkMobile(): void {
+    this.isMobile = window.innerWidth < 768;
+  }
+
+  get isCollapsed(): boolean {
+    return this.sidebarService.isCollapsed();
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { appConfig } from './app/app.config';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(App, {
   ...appConfig,
   providers: [
     ...(appConfig.providers || []),
     provideAnimations(), // required for Angular animations
+    provideRouter(routes),
   ],
 }).catch((err) => console.error(err));
