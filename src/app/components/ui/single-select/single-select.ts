ff1ae815345f0a0a79077f1cb8e3411772dbb794
Ahmadullah Mukhlis <nasarimukhlis@gmail.com>
2026-01-15 19:58:38 +0430

    the ng modal is created for all the system

---
+++ src/app/components/ui/single-select/single-select.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-select',
+  imports: [],
+  templateUrl: './single-select.html',
+  styleUrl: './single-select.css',
+})
+export class SingleSelect {
+
+}
