f9db9e61e712a65408acd54e91dd08e6252f903d
Ahmadullah Mukhlis <nasarimukhlis@gmail.com>
2026-01-16 10:56:53 +0430

    the theformbuilder is make

--- src/app/components/ui/dynamic-form-builder/dynamic-form-builder.spec.ts
+++ src/app/components/ui/dynamic-form-builder/dynamic-form-builder.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FormBuilder } from './form-builder';
+import { DynamicFormBuilder } from './dynamic-form-builder';
 
-describe('FormBuilder', () => {
-  let component: FormBuilder;
-  let fixture: ComponentFixture<FormBuilder>;
+describe('DynamicFormBuilder', () => {
+  let component: DynamicFormBuilder;
+  let fixture: ComponentFixture<DynamicFormBuilder>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FormBuilder]
+      imports: [DynamicFormBuilder]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(FormBuilder);
+    fixture = TestBed.createComponent(DynamicFormBuilder);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
