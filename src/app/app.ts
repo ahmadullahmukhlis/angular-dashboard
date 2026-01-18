7e51fbeefb050458c0165146954b5c0a8a6fddad
Ahmadullah Mukhlis <nasarimukhlis@gmail.com>
2026-01-18 19:21:51 +0430

    teh problem fotheis fxed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { SidebarService } from './services/sidebar.service';
 import { MainContent } from './components/layout/main-content/main-content';
-
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
-  imports: [MainContent],
+  imports: [MainContent,ToastModule,ConfirmDialogModule],
 })
 export class App {
   title = 'Metric Dashboard';
