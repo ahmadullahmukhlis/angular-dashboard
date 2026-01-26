22a7dd01ad06143edb8b823d6ec6d37ec66b7bbb
Ahmadullah Mukhlis <nasarimukhlis@gmail.com>
2026-01-26 21:32:12 +0430

    The problem fothe is fxed

---
+++ src/app/components/ui/toast.component/toast.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {ToastModule} from 'primeng/toast'
+@Component({
+  selector: 'app-toast',
+  imports: [ToastModule],
+  templateUrl: './toast.component.html',
+  styleUrl: './toast.component.css',
+})
+export class ToastComponent {
+
+}
