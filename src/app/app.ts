29ed4e7e98cf6a669cb6dde27459ef4b6868ebb0
Ahmadullah Mukhlis <nasarimukhlis@gmail.com>
2026-01-26 21:36:28 +0430

    the problem fot heis fxed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { SidebarService } from './services/sidebar.service';
-import { ToastModule } from 'primeng/toast';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { RouterModule } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
-  imports: [ToastModule, ConfirmDialogModule, RouterModule], // remove MainContent import!
+  imports: [  RouterModule], // remove MainContent import!
 })
 export class App {
   title = ' Dashboard';
