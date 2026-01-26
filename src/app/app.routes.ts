9e7963eb0d1a85bffbc4fed1cce7bdce9decd1bc
Ahmadullah Mukhlis <nasarimukhlis@gmail.com>
2026-01-26 22:08:29 +0430

    the problem fothe isf xed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,27 @@
 import { Routes } from '@angular/router';
-import { Dashboard } from './pages/dashboard/dashboard';
 import { AuthGuard } from './guards/auth.guard';
 import { Login } from './pages/login/login';
 import { MainContent } from './components/layout/main-content/main-content';
 
 export const routes: Routes = [
-  { path: 'login', component: Login }, // login has no layout
+  { path: 'login', component: Login },
 
   {
     path: '',
     component: MainContent, 
     canActivate: [AuthGuard],
     children: [
-      { path: '', component: Dashboard },
-      { path: 'profile', component: Dashboard },
-      // other protected pages here
+      { 
+        path: '', 
+        // FIX: Use loadComponent to lazy load the page
+        loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard) 
+      },
+      { 
+        path: 'profile', 
+        loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.Dashboard) 
+      },
     ]
   },
 
-  { path: '**', redirectTo: '' } // fallback
+  { path: '**', redirectTo: '' }
 ];
