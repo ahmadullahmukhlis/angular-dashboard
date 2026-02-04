d04cf9b24617015acdeeffc9dfde016ffd511260
ahmadullahmukhlis <nasarimukhlis@email.com>
2026-02-04 09:53:38 +0430

    the problem fo the si fxed for the system

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
   private http = inject(HttpClient);
-  private BASE_URL = import.meta.env.NG_APP_API_URL;
+  private BASE_URL = this.endsWithSlash(import.meta.env.NG_APP_API_URL);
 
   private getHeaders(isFormData: boolean = false) {
     const token = localStorage.getItem('token');
@@ -22,35 +22,50 @@ export class ApiService {
 
     return headers;
   }
+  // Check if string starts with "/"
+  private startsWithSlash(value: string): String {
+    const isBool = value.startsWith('/');
+    if (isBool) {
+      return value;
+    }
+    return '/' + value;
+  }
+  private endsWithSlash(value: string): string {
+    const isBool = value.endsWith('/');
+    if (isBool) {
+      return value;
+    }
+    return value + '/';
+  }
 
   get<T>(url: string, params: Record<string, any> = {}): Observable<T> {
-    return this.http.get<T>(`${this.BASE_URL}${url}`, {
+    return this.http.get<T>(`${this.BASE_URL}${this.startsWithSlash(url)}`, {
       headers: this.getHeaders(),
       params: params,
     });
   }
 
   post<T>(url: string, body: any): Observable<T> {
-    return this.http.post<T>(`${this.BASE_URL}${url}`, body, {
+    return this.http.post<T>(`${this.BASE_URL}${this.startsWithSlash(url)}`, body, {
       headers: this.getHeaders(),
     });
   }
 
   put<T>(url: string, body: any): Observable<T> {
-    return this.http.put<T>(`${this.BASE_URL}${url}`, body, {
+    return this.http.put<T>(`${this.BASE_URL}${this.startsWithSlash(url)}`, body, {
       headers: this.getHeaders(),
     });
   }
 
   delete<T>(url: string): Observable<T> {
-    return this.http.delete<T>(`${this.BASE_URL}${url}`, {
+    return this.http.delete<T>(`${this.BASE_URL}${this.startsWithSlash(url)}`, {
       headers: this.getHeaders(),
     });
   }
   request<T>(method: string, url: string, body: any): Observable<HttpEvent<T>> {
     const isFormData = body instanceof FormData;
 
-    return this.http.request<T>(method, `${this.BASE_URL}${url}`, {
+    return this.http.request<T>(method, `${this.BASE_URL}${this.startsWithSlash(url)}`, {
       body: body,
       headers: this.getHeaders(isFormData),
       observe: 'events', // Required for progress bars
