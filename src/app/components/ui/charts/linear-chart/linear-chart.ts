f9d04cf154afc1817f060ab69e69ae7de2df300c
Ahmadullah Mukhlis <yourgmail@gmail.com>
2026-02-08 14:31:28 +0430

    the problem fothe si fxed for the system

---
+++ src/app/components/ui/charts/linear-chart/linear-chart.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { IAxisChart } from '../../../../models/chart.model';
+import { NgApexchartsModule } from 'ng-apexcharts';
+
+@Component({
+  selector: 'app-linear-chart',
+  imports: [NgApexchartsModule],
+  templateUrl: './linear-chart.html',
+  styleUrl: './linear-chart.css',
+})
+export class LinearChart {
+  @Input() config!: IAxisChart;
+  @Input() title: string = 'Linear Data';
+}
