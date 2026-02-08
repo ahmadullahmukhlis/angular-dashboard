4c8ff344fafa5adda19f0de5de0d8fa0f15ff4cc
Ahmadullah Mukhlis <yourgmail@gmail.com>
2026-02-08 15:24:37 +0430

    the problem of the sif xed for the ngOnit

--- src/app/components/ui/charts/linear-chart/linear-chart.ts
+++ src/app/components/ui/charts/linear-chart/linear-chart.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 import { IAxisChart } from '../../../../models/chart.model';
 import { NgApexchartsModule } from 'ng-apexcharts';
 
@@ -11,4 +11,11 @@ import { NgApexchartsModule } from 'ng-apexcharts';
 export class LinearChart {
   @Input() config!: IAxisChart;
   @Input() title: string = 'Linear Data';
+  @Input() isLoading?: boolean = false;
+  loading = false;
+  ngOnInit() {
+    if (this.isLoading !== undefined) {
+      this.loading = this.isLoading;
+    }
+  }
 }
