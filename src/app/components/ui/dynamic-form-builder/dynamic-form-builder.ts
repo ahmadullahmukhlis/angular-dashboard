656a59198baf54777e6e7c00258b069c4656b440
Ahmadullah Mukhlis <yourgmail@gmail.com>
2026-02-10 09:25:15 +0430

    the form build task

--- src/app/components/ui/dynamic-form-builder/dynamic-form-builder.ts
+++ src/app/components/ui/dynamic-form-builder/dynamic-form-builder.ts
@@ -222,7 +222,7 @@ export class DynamicFormBuilderComponent implements OnChanges {
 
     if (this.formSubmitted.observed) {
       this.formSubmitted.emit(payload);
-      this.loading = false;
+      this.resetLoadingState();
       return;
     }
 
