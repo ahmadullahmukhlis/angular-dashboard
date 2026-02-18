bffff79781d19b7ce8d1a4360273d27a3b99fcaa
Ahmadullah Mukhlis <nasarimukhlis@gmail.com>
2026-02-18 19:28:43 +0430

    the problem of theis fxed

--- src/app/shared/SIdeBarRoutes.ts
+++ src/app/shared/SIdeBarRoutes.ts
@@ -11,40 +11,22 @@ export const SIDEBAR_ROUTES: SidebarItem[] = [
   },
   {
     id: 2,
-    label: 'Apps',
+    label: 'User Mangement',
     icon: 'fa-th-large',
     route: '/apps',
     isActive: false,
     isExpanded: false,
     children: [
-      { id: 21, label: 'Chat', icon: 'fa-comments', route: '/apps/chat', isActive: false },
-      { id: 22, label: 'Email', icon: 'fa-envelope', route: '/apps/email', isActive: false },
+      { id: 21, label: 'Clients', icon: 'fa-comments', route: '/user-management/clients', isActive: false },
+      { id: 22, label: 'Users', icon: 'fa-envelope', route: '/user-management/clients', isActive: false },
       {
         id: 23,
-        label: 'Calendar',
+        label: 'Roles',
         icon: 'fa-calendar-alt',
-        route: '/apps/calendar',
+        route: '/user-management/roles',
         isActive: false,
       },
     ],
   },
-  {
-    id: 3,
-    label: 'Pages',
-    icon: 'fa-file-alt',
-    route: '/pages',
-    isActive: false,
-    isExpanded: false,
-    children: [
-      { id: 31, label: 'Users', icon: 'fa-users', route: '/pages/users', isActive: false },
-      {
-        id: 32,
-        label: 'Profile',
-        icon: 'fa-user-circle',
-        route: '/pages/profile',
-        isActive: false,
-      },
-      { id: 33, label: 'Settings', icon: 'fa-cog', route: '/pages/settings', isActive: false },
-    ],
-  },
+
 ];
