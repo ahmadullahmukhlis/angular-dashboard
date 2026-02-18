7d327da7ac9ecb0c113ee2d09c32da4ba44c9581
Ahmadullah Mukhlis <nasarimukhlis@gmail.com>
2026-02-18 19:48:00 +0430

    the problem of tehis fxed for eh stsystem

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,16 +1,18 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpEvent, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from '../auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
   private http = inject(HttpClient);
+  private authService = inject(AuthService);
   private BASE_URL = this.endsWithSlash(import.meta.env.NG_APP_API_URL);
 
   private getHeaders(isFormData: boolean = false) {
-    const token = localStorage.getItem('token');
+    const token = this.authService.getAccessToken();
     let headers = new HttpHeaders({
       Authorization: token ? `Bearer ${token}` : '',
     });
